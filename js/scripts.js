2ea951a5071606ed3fc38b9ee079c4fe6a151d17
sykey <basuraxam@gmail.com>
2019-01-02 23:44:24 +0100

    Ajustes codigo

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+function servWorker(){
+      // Set up service worker
+      if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('service-worker.js', {scope: '.'});
+      }
+      // Easter Egg to get it full black
+      // Awesome for OLED screens
+      if (localStorage.getItem('isDeepBlack')) {
+            document.body.classList.add('deepblack');
+      }        
+} 
